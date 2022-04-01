6d2f7a706de079029fa241540e0e823708d181c8
Johnathan Graybeal <graybealjohnathan@outlook.com>
2022-04-01 19:51:04 -0400

    remove images

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,37 +11,41 @@ import { Logo } from "../components/Logo";
 import { Text } from "@chakra-ui/react";
 
 //Need to have custom.d.ts open in another tab for these to not have an error for some reason
-import doge from '../images/doge.jpg'
-import nelson from '../images/nelson.jpg'
+import doge from "../images/doge.jpg";
+import nelson from "../images/nelson.jpg";
 
 const Index = () => (
   <Container height="100vh">
     <Nav />
-    <Main
-    alignItems={"center"}
-    width={"90%"}
-    height={"100vh"}>
-    <Hero />
-    
-    <Introduction introtext={"Welcome to the best pet store that has ever existed on the face of planet Earth"} />
-    <Introduction introtext={"If you leave now, you may never feel real love in your entire short lifetime, so stick around ;)"} />
+    <Main alignItems={"center"} width={"90%"} height={"100vh"}>
+      <Hero />
 
-    <IntroImage alttext={"Doge"} imagepath={doge} />
+      <Introduction
+        introtext={
+          "Welcome to the best pet store that has ever existed on the face of planet Earth"
+        }
+      />
+      <Introduction
+        introtext={
+          "If you leave now, you may never feel real love in your entire short lifetime, so stick around ;)"
+        }
+      />
 
-    <Text
-      alignItems={"flex-start"}
-      justifyContent="center"
-      height="25vh"
-      bgClip="text"
-      fontSize="2vw"
-      fontWeight='bold'
-      margin="50px"
-      color={useColorModeValue("gray.900", "gray.100")}>
-      {"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras sem nulla, euismod id lobortis nec, egestas id libero. Nullam faucibus, arcu sit amet rutrum feugiat, ipsum est consequat urna, sit amet lacinia tortor magna nec mauris. Nullam at nisl bibendum tellus posuere bibendum. Nunc a ex ut nibh dapibus porta. Donec et pretium quam. Nunc ac lectus tincidunt, luctus leo et, vehicula augue. Proin iaculis ex in lectus molestie, ac rhoncus nisl tristique. "}
-    </Text>
-
-    <IntroImage alttext={"Nelson"} imagepath={nelson} />
-    </Main> 
+      <Text
+        alignItems={"flex-start"}
+        justifyContent="center"
+        height="25vh"
+        bgClip="text"
+        fontSize="2vw"
+        fontWeight="bold"
+        margin="50px"
+        color={useColorModeValue("gray.900", "gray.100")}
+      >
+        {
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras sem nulla, euismod id lobortis nec, egestas id libero. Nullam faucibus, arcu sit amet rutrum feugiat, ipsum est consequat urna, sit amet lacinia tortor magna nec mauris. Nullam at nisl bibendum tellus posuere bibendum. Nunc a ex ut nibh dapibus porta. Donec et pretium quam. Nunc ac lectus tincidunt, luctus leo et, vehicula augue. Proin iaculis ex in lectus molestie, ac rhoncus nisl tristique. "
+        }
+      </Text>
+    </Main>
     <Footer />
   </Container>
 );
